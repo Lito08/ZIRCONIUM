642d9599e94651490cec02242f46fa0b116f1ec1
mhmnal <mhmn.inabox@gmail.com>
2021-10-17 13:43:40 +0800

    Order profile done

--- profileorder.php
+++ profileorder.php
@@ -1,46 +1,31 @@
 <?php 
 session_start();
 
+	include("products/includes/config.php");
     include("connection.php");
     include("functions.php");
 
-?>
-    
-<?php
-$user_id = $_SESSION['user_id'];
-$sql = "SELECT *FROM users WHERE user_id = '$user_id'; ";
-$result = mysqli_query($con, $sql);
-if(mysqli_num_rows($result) > 0)
-{
-	foreach($result as $row)
+
+	$sessid = $_SESSION['user_id'];
+	$query = "SELECT * FROM sale WHERE User = '$sessid';";
+	$results = mysqli_query($con, $query) or die (mysqli_query());
+
+	if(mysqli_num_rows($results) == 0)
 	{
-		
+		echo '<div id="content" class="col-md-7"><div align="center"><h3>Your cart is empty.</h3> You can find our items on our <a href="index.php">product page</a>.</div></div><div class="col-md-7"></div>';
 	}
-}
-else
-{
-	echo "no record found";
-}
-
+	else
+	{
 ?>
-
-
-
-
 
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<meta http-equiv="pragma" content="no-cache" />
-<meta http-equiv="cache-control" content="max-age=604800" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="author" content="Bootstrap-ecommerce by Vosidiy M.">
 
-
-
-<title>Profile</title>
-
-<link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+<title>Zirconium - Shopping Cart</title>
 
 <!-- jQuery -->
 <script src="js/jquery-2.0.0.min.js" type="text/javascript"></script>
@@ -49,111 +34,102 @@ else
 <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
 
-<!-- Font awesome 5 -->
+<!-- Fonticons -->
 <link href="fonts/fontawesome/css/all.min.css" type="text/css" rel="stylesheet">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto&display=swap" rel="stylesheet"> 
-
-<style>
-
-
-
-
-</style>
+<link href="fonts/feathericons/css/iconfont.css" rel="stylesheet" type="text/css" />
+<link href="fonts/material-icons/css/materialdesignicons.css" rel="stylesheet" type="text/css" />
 
 <!-- custom style -->
 <link href="css/ui.css" rel="stylesheet" type="text/css"/>
-<link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+<link href="css/responsive.css" rel="stylesheet" />
 
 <!-- custom javascript -->
 <script src="js/script.js" type="text/javascript"></script>
 
-<script type="text/javascript">
-/// some script
-
-// jquery ready start
-$(document).ready(function() {
-	// jQuery code
-
-}); 
-// jquery end
-</script>
 
 </head>
 <body>
 
-
+<!-- ========================= HEADER  ========================= -->
 <?php include('includes/header.php') ?>
 
 <!-- ========================= SECTION PAGETOP ========================= -->
-<section class="section-pagetop bg-primary">
+<section class="section-pagetop bg">
 <div class="container" >
-	<h2 class="title-page text-white" style="font-family:Roboto; text-transform: uppercase">MY orders</h2>
+	<h2 class="title-page" style="font-family:Roboto; text-transform: uppercase">my orders</h2>
 </div> <!-- container //  -->
 </section>
 <!-- ========================= SECTION INTRO END// ========================= -->
 
-
-
-<!-- ========================= SECTION CONTENT ========================= -->
-<section class="section-content padding-y">
+<section class="section-content padding-y bg">
 <div class="container">
 
+
+<!-- ============================ COMPONENT 2  ================================= -->
 <div class="row">
 	<aside class="col-md-3">
 		<ul class="list-group">
 			<a class="list-group-item" href="profile.php"> Account overview  </a>
-			<a class="list-group-item active" href="#"> My Orders </a>
+			<a class="list-group-item active" href="profileorder.php"> My Orders </a>
 			<a class="list-group-item" href="shoppingcart.php"> My wishlist </a>
-			<a class="list-group-item" href="#"> Return and refunds </a>
 		</ul>
 	</aside> <!-- col.// -->
+	
 	<main class="col-md-9">
+	<article class="card  mb-3">
+	<div class="card-body">
+			
+				<h5 class="card-title mb-4">Recent orders </h5>					
+	
+	<?php
+	$total = 0;
+	$useremail=$_SESSION['user_id'];
+	$sql = "SELECT products.Vimage1 as Vimage1,products.price,products.title,products.id as pid,type.typename,sale.id,sale.country,sale.dop as rice from sale join products on sale.item=products.id join type on type.id=products.ptype where sale.User=:useremail";
+	$query = $dbh -> prepare($sql);
+	$query-> bindParam(':useremail', $useremail, PDO::PARAM_STR);
+	$query->execute();
+	$results=$query->fetchAll(PDO::FETCH_OBJ);
+	$cnt=1;
+	if($query->rowCount() > 0)
+	{
+	foreach($results as $result)
+	{  ?>
+			<article class="card-body border-bottom">
+					
+						<div class="col-md-7">																							
+										<div class="col-md-6">
+										<figure class="itemside  mb-3">
+											<div class="aside">
+												<img src="superadmin/img/<?php echo htmlentities($result->Vimage1);?>" class="border img-sm">
+											</div>
+											<figcaption class="info">
+												<a  name="name" class="title"><?php echo htmlentities($result->title);?></a>
+												<strong name="price" class="">RM<?php echo htmlentities($result->price);?></strong>												
+											</figcaption>
+										</figure>
+									</form>
+								</figcaption>
+							</figure> 
+						</div>					
+						</div> <!-- col.// -->										
+			</article> <!-- card-group.// -->
+			<?php }} ?>		
+	</div>
+	</article>
+	</main>
+</div> <!-- card.// -->
 
-		<article class="card  mb-3">
-			<div class="card-body">
-				<h5 class="card-title mb-4">List of Orders </h5>	
-
-				<div class="row">
-				<div class="col-md-6">
-					<figure class="itemside  mb-3">
-						<div class="aside"><img src="images/items/1.jpg" class="border img-sm"></div>
-						<figcaption class="info">
-							<time class="text-muted"><i class="fa fa-calendar-alt"></i> 12.09.2019</time>
-							<p>Great item name goes here </p>
-							<span class="text-warning">Pending</span>
-						</figcaption>
-					</figure>
-				</div> <!-- col.// -->
-				<div class="col-md-6">
-					<figure class="itemside  mb-3">
-						<div class="aside"><img src="images/items/2.jpg" class="border img-sm"></div>
-						<figcaption class="info">
-							<time class="text-muted"><i class="fa fa-calendar-alt"></i> 12.09.2019</time>
-							<p>Machine for kitchen to blend </p>
-							<span class="text-success">Departured</span>
-						</figcaption>
-					</figure>
-				</div> <!-- col.// -->
-				<div class="col-md-6">
-					<figure class="itemside mb-3">
-						<div class="aside"><img src="images/items/3.jpg" class="border img-sm"></div>
-						<figcaption class="info">
-							<time class="text-muted"><i class="fa fa-calendar-alt"></i> 12.09.2019</time>
-							<p>Ladies bag original leather </p>
-							<span class="text-success">Shipped  </span>
-						</figcaption>
-					</figure>
-				</div> <!-- col.// -->
-			</div> <!-- row.// -->
 
 
-			</div> <!-- card-body .// -->
-		</article> <!-- card.// -->
+<!-- ============================ COMPONENT 2 END .// ================================= -->
 
-	</main> <!-- col.// -->
-</div>
+
+	</aside> <!-- col.// -->
+</div> <!-- row.// -->
+
+<br>
+<br>
+
 
 </div> <!-- container .//  -->
 </section>
@@ -161,22 +137,8 @@ $(document).ready(function() {
 
 
 <!-- ========================= FOOTER ========================= -->
-<footer class="section-footer border-top padding-y">
-	<div class="container">
-		<p class="float-md-right"> 
-			&copy Copyright 2019 All rights reserved
-		</p>
-		<p>
-			<a href="#">Terms and conditions</a>
-		</p>
-	</div><!-- //container -->
-</footer>
-<!-- ========================= FOOTER END // ========================= -->
-
-
+<?php include('includes/footer.php') ?>
 
 </body>
 </html>
-
-
-
+<?php } ?>
